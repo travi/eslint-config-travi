6772295626e2e1c8504d4bd17e856d83c15750e6
Matt Travi <programmer@travi.org>
2017-03-11 00:43:30 -0600

    fix(require-default-props): disabled require-default-props
    this rule would only make sense conditionally. i like the idea of preventing default function props,
    but not at the cost of preventing all optional props

--- rules/react.js
+++ rules/react.js
@@ -24,6 +24,7 @@ module.exports = {
     'react/jsx-pascal-case': 'error',
     'react/jsx-uses-react': 'error',
     'react/jsx-uses-vars': 'error',
-    'react/jsx-wrap-multilines': 'error'
+    'react/jsx-wrap-multilines': 'error',
+    'react/require-default-props': 'off'
   }
 };
