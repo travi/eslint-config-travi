94eeab2a9a0c7df92f53a689eb8ab554e370b30c
Matt Travi <developer@travi.org>
2018-06-25 17:24:49 -0500

    fix(operator-linebreak): overrode the airbnb override to get the default

--- rules/es6.js
+++ rules/es6.js
@@ -52,6 +52,7 @@ module.exports = {
     'func-names': 'error',
     'no-underscore-dangle': ['error', {allow: ['_links', '_embedded']}],
     'no-unused-vars': ['error', {ignoreRestSiblings: true}],
-    'no-multi-spaces': ['error', {ignoreEOLComments: true}]
+    'no-multi-spaces': ['error', {ignoreEOLComments: true}],
+    'operator-linebreak': ['error', 'after', {overrides: {'?': 'before', ':': 'before'}}]
   }
 };
